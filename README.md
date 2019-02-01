7ea2b519b9a3a25cae57462ab8234497cb386726
Yangzhedi <uiryzd@163.com>
2019-02-01 11:39:32 +0800

    fix(webpack): add some dependencies

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # my-site-fe
 new💊 personal website FE:React
 
-[![ENV](https://img.shields.io/badge/react-^16.7.0-alpha.2-blue.svg)](https://github.com/Yangzhedi/myBlog)
+[![ENV](https://img.shields.io/badge/react-%5E16.7.0--alpha.2-blue.svg)](https://github.com/Yangzhedi/my-site-fe)
--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
 import React, { Component } from 'react';
+import { Menu, Icon } from 'antd';
+
 import logo from '@/assets/logo.svg';
 import '@/App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      current: 'home',
+    }
+  }
+  handleClick = (e) => {
+    console.log('click ', e);
+    this.setState({
+      current: e.key,
+    });
+  };
   render() {
     return (
       <div className="App">
+        <Menu onClick={this.handleClick}
+              selectedKeys={[this.state.current]}
+              mode="horizontal"
+        >
+          <Menu.Item key="home">
+            <Icon type="home" /> 主页
+          </Menu.Item>
+          <Menu.Item key="blog">
+            <Icon type="book" /> 博客
+          </Menu.Item>
+          <Menu.Item key="stock">
+            <Icon type="file-excel" /> 数据
+          </Menu.Item>
+          <Menu.Item key="about">
+            <Icon type="solution" /> 关于
+          </Menu.Item>
+          <Menu.Item key="code-guide">
+            <Icon type="code-o" /> 规范
+          </Menu.Item>
+          <Menu.Item key="gif-maker" disabled>
+            <Icon type="picture" /> 生成表情包
+          </Menu.Item>
+        </Menu>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
